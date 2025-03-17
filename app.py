de5cf9957364a59df2f83c15a8a55b5adfc135ee
Zeeshan <zeehan.riasat@planlabsolutions.com>
2025-03-17 17:11:38 +0500

    formated strings

--- app.py
+++ app.py
@@ -13,6 +13,13 @@ print(course)
 
 first_name = "Zeeshan"
 last_name = "Riasat"
-full_name = first_name + " " + last_name
+full_name = first_name + " " + last_name  # string concatination
+
+print(full_name)
+
+# formated stirngs
+first_name = "Zeeshan"
+last_name = "Riasat"
+full_name = f"{first_name} {last_name}"
 
 print(full_name)
