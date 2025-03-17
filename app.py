d0ea0f3b51429e0729dcf0fa99516c364ad73209
Zeeshan <zeehan.riasat@planlabsolutions.com>
2025-03-17 17:32:23 +0500

    string methods

--- app.py
+++ app.py
@@ -23,3 +23,16 @@ last_name = "Riasat"
 full_name = f"{first_name} {last_name}"
 
 print(full_name)
+
+# String methods
+
+print(course.upper())
+print(course.lower())
+print(course.title())
+# trim any white space form start and end of string we also have lstrip() and rstrip()
+print(course.strip())
+print(course.find("pro"))  # return index of charctor
+print("pro" in course)  # return true false
+print("pro" not in course)  # return true false
+
+print(course.replace('p', 'j'))
