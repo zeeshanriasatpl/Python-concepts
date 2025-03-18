c0de523d719f2ced08feeac4fcaf97dd733d3d81
Zeeshan <link.zeeshan.riasat@gmail.com>
2025-03-18 13:42:04 +0500

    conditional statements

--- app.py
+++ app.py
@@ -12,3 +12,13 @@ print(f"x: {x}, y: {y}")
 
 # Falsy values => "", 0, None
 print(bool(""))
+
+# conditional statements
+temperature = 35
+if temperature > 30:
+    print("Its warm")
+elif temperature > 30:
+    print("its nice")
+else:
+    print("its cold")
+print("Done")
