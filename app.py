fd8c08afe7e8811d9ffd3a967ff0cc28df386662
Zeeshan <link.zeeshan.riasat@gmail.com>
2025-03-19 17:53:07 +0500

    Exercise

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-command = ""
-
-while command.lower() != "quit":
-    command = input(">: ")
-    print("ECHO: ", command)
+# Exercise
+count = 0
+for number in range(1, 10):
+    if (number % 2 == 0):
+        count = count + 1
+        print("number: ", number, " count:", count)
+print(f"we have {count} even numbers")
